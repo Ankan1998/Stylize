bf372e3c7adef88d45971bafbe45a33d539fb7c2
Ankan Sharma <ankanroni3@gmail.com>
2021-07-14 19:30:46 +0530

    Started with style.py to load model and inference

---
+++ neural_style/style.py
@@ -0,0 +1,17 @@
+import argparse
+import os
+import sys
+import time
+import re
+
+import numpy as np
+import torch
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+import torch.onnx
+
+import utils
+from transformer_net import TransformerNet
+from vgg import Vgg16
