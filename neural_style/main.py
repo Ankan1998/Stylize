ce211435cadcec314bcb44e10ffc4ee87be62578
Ankan Sharma <ankanroni3@gmail.com>
2021-07-14 21:47:01 +0530

    file upload and code restructure in main

--- neural_style/main.py
+++ neural_style/main.py
@@ -1,15 +1,17 @@
 import streamlit as st
 from PIL import Image
 import os
-
+from tempfile import TemporaryDirectory
 import style
+
+
+
 
 st.title("Style Transfer with Pytorch")
 
-img = st.sidebar.selectbox(
-    "Select image", 
-    ("amber.jpg",)
-)
+
+
+uploaded_file = st.file_uploader("Upload Image",type=['png','jpeg','jpg'])
 
 style_name = st.sidebar.selectbox(
     "Select style image", 
@@ -17,19 +19,21 @@ style_name = st.sidebar.selectbox(
 )
 
 model = os.path.join('saved_models', style_name + '.pth')
-input_img = "images/content-images/" + img
-out_img = "images/output-images" + style_name + "-" + img
+#  with TemporaryDirectory(prefix="myapp-") as tmpdir:
+if uploaded_file is not None:
 
-st.write("### Source Image:")
-image = Image.open(input_img)
-st.image(image, width = 400, use_column_width=True)
+    st.write("### Source Image:")
+    image = Image.open(uploaded_file)
+    st.image(image, width = 300, use_column_width=True)
 
 clicked = st.button("Run Style Transfer")
 if clicked:
     model = style.load_model_style_transfer(model)
-    style.stylize(model, input_img, out_img)
+    out_img = style.stylize(model, image)
 
     st.write("### Stylized Image:")
+    img = Image.fromarray(out_img)
+    img.save(filename)
     image = Image.open(out_img)
     st.image(image, width = 400, use_column_width=True)
 
