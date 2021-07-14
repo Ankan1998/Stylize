abec8945e1b6ef7502fa3f159d17ca90beb61250
Ankan Sharma <ankanroni3@gmail.com>
2021-07-14 20:24:36 +0530

    basic streamlit app in main.py completed

--- neural_style/main.py
+++ neural_style/main.py
@@ -6,14 +6,34 @@ import style
 
 st.title("Style Transfer with Pytorch")
 
-
+img = st.sidebar.selectbox(
+    "Select image", 
+    ("amber.jpg",)
+)
 
 style_name = st.sidebar.selectbox(
     "Select style image", 
-    ("candy", "mosaic", "udnie")
+    ("candy", "mosaic", "rain_princess", "udnie")
 )
 
 model = os.path.join('saved_models', style_name + '.pth')
+input_img = "images/content-images/" + img
+out_img = "images/output-images" + style_name + "-" + img
+
+st.write("### Source Image:")
+image = Image.open(input_img)
+st.image(image, width = 400, use_column_width=True)
+
+clicked = st.button("Run Style Transfer")
+if clicked:
+    model = style.load_model_style_transfer(model)
+    style.stylize(model, input_img, out_img)
+
+    st.write("### Stylized Image:")
+    image = Image.open(out_img)
+    st.image(image, width = 400, use_column_width=True)
+
+
 
 #print(model)
 # r'\saved_models\candy.pth
